6ebfe0d22cc03747de773125850920debac5cc2d
BuildTools <unconfigured@null.spigotmc.org>
2018-09-16 14:08:29 +1000

    Implemented network test classes (Creating a bucket, downloading and uploading files)

---
+++ SIT313A2/SIT313A2/CreateStorageBucket.cs
@@ -0,0 +1,34 @@
+﻿using Google.Cloud.Storage.V1;
+using System;
+using System.Diagnostics;
+
+namespace SIT313A2
+{
+    class CreateStorageBucket
+    {
+        static void Main(string[] args)
+        {
+            // Google Cloud Platform project ID.
+            string projectId = "sit313-2018";
+
+
+            // Instantiates a client.
+            StorageClient storageClient = StorageClient.Create();
+
+            // The name for the new bucket.
+            string bucketName = projectId + "-test-bucket";
+            try
+            {
+                // Creates the new bucket.
+                storageClient.CreateBucket(projectId, bucketName);
+                Console.WriteLine($"Bucket {bucketName} created.");
+            }
+            catch (Google.GoogleApiException e)
+            when (e.Error.Code == 409)
+            {
+                // The bucket already exists.  That's fine.
+                Console.WriteLine(e.Error.Message);
+            }
+        }
+    }
+}
