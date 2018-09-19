75f5321133e1fcac3bc1cf9b00c6de93e9dd5527
Joshua Pujol <jpujol@deakin.edu.au>
2018-09-20 09:25:45 +1000

    Debugging permissions

--- SIT313A2/SIT313A2/MainPage.xaml.cs
+++ SIT313A2/SIT313A2/MainPage.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,8 +19,54 @@ namespace SIT313A2
 			InitializeComponent();
 		}
 
+
+
         public void OnButtonClicked(object sender, EventArgs args)
         {
         }
+        public async void TakePhotoButton_OnClicked(object sender, EventArgs e)
+        {
+
+            var cameraStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
+            var storageStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Storage);
+
+            if (cameraStatus != PermissionStatus.Granted || storageStatus != PermissionStatus.Granted)
+            {
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(new[] { Permission.Camera, Permission.Storage });
+                cameraStatus = results[Permission.Camera];
+                storageStatus = results[Permission.Storage];
+            }
+
+            if (cameraStatus == PermissionStatus.Granted && storageStatus == PermissionStatus.Granted)
+            {
+                var file = await CrossMedia.Current.TakePhotoAsync(
+                    new StoreCameraMediaOptions
+                    {
+                        SaveToAlbum = true,
+                        Name = "Test.jpg"
+                    });
+                if (file == null)
+                    return;
+
+                MainImage.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    file.Dispose();
+                    return stream;
+                });
+
+            }
+            else
+            {
+                await DisplayAlert("Permissions Denied", "Unable to take photos.", "OK");
+                //On iOS you may want to send your user to the settings screen.
+                //CrossPermissions.Current.OpenAppSettings();
+            }
+
+
+
+
+        }
+
         }
 }
